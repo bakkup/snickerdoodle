4754664b941c065937de66023d890507aadf8685
Adib Yahaya <adib.yhy@gmail.com>
2019-04-21 17:10:43 -0400

    Add refresh timer
    Updates UI every 1 second

--- firstflight-drone/gui/qt/gui.py
+++ firstflight-drone/gui/qt/gui.py
@@ -3,6 +3,7 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from mainWindow import Ui_MainWindow
 from diagnostics import Ui_widget_diagnostics
+from adc import *
 
 class MainWindow(QMainWindow):
   def __init__(self):
@@ -25,6 +26,15 @@ class MainWindow(QMainWindow):
     self.diagnosticsDock.setMinimumSize(302, 0)
     self.diagnostics.setupUi(self.diagnosticsDock)
     self.addDockWidget(Qt.LeftDockWidgetArea, self.diagnosticsDock)
+
+  def timerSetup(self):
+    self.timer = QTimer()
+    self.timer.timeout.connect(self.tick)
+    self.timer.start(1000)
+
+  def tick(self):
+    self.busVoltage = ReadADC()
+    # self.diagnostics.
 
 def main():
   app = QApplication(sys.argv)
