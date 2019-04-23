450e6014bbec5609bc588f5041155d462752409d
adibyhy <adibyhy@Desktop-Mint>
2019-04-23 18:43:30 -0400

    Read X acceleration from /dev/mem

--- firstflight-drone/gui/qt/attitude.py
+++ firstflight-drone/gui/qt/attitude.py
@@ -6,10 +6,23 @@ import struct
 
 IMU_ADDRESS = 0x43c20000
 
+# Change to X acceleration if getting roll angle is not possible
 def ReadRoll():
-    try:
+  try:
     f = open("/dev/mem", "r+b")
     mem = mmap.mmap(f.fileno(), 100, offset=IMU_ADDRESS)
 
     mem.seek(0)
     fromMem = struct.unpack('h', mem.read(2))[0]
+    # print "Roll" + " = " + str(fromMem)
+
+    # Close mem after finishing
+    mem.close()
+    f.close()
+
+    result = fromMem
+  except:
+    result = 0
+    print "Error in reading memory (attitude.ReadRoll)"
+
+  return result
