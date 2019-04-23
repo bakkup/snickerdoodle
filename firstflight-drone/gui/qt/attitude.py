0f20f3c7a818e183456eaebf61fa061b9e069bb1
adibyhy <adibyhy@Desktop-Mint>
2019-04-23 18:58:25 -0400

    Read Y acceleration from /dev/mem

--- firstflight-drone/gui/qt/attitude.py
+++ firstflight-drone/gui/qt/attitude.py
@@ -13,6 +13,7 @@ def ReadRoll():
     mem = mmap.mmap(f.fileno(), 100, offset=IMU_ADDRESS)
 
     mem.seek(0)
+    # imuAccelX = 'X Accel: ' + str(round((struct.unpack('h', mem.read(2))[0] / 1670.70),2)) + ' m/sec^2'
     fromMem = struct.unpack('h', mem.read(2))[0]
     # print "Roll" + " = " + str(fromMem)
 
@@ -24,5 +25,27 @@ def ReadRoll():
   except:
     result = 0
     print "Error in reading memory (attitude.ReadRoll)"
+
+  return result
+
+# Change to Y acceleration if getting pitch angle is not possible
+def ReadPitch():
+  try:
+    f = open("/dev/mem", "r+b")
+    mem = mmap.mmap(f.fileno(), 100, offset=IMU_ADDRESS)
+
+    mem.seek(4)
+    # imuAccelY = 'Y Accel: ' + str(round((struct.unpack('h', mem.read(2))[0] / -1670.70),2)) + ' m/sec^2'
+    fromMem = struct.unpack('h', mem.read(2))[0]
+    # print "Pitch" + " = " + str(fromMem)
+
+    # Close mem after finishing
+    mem.close()
+    f.close()
+
+    result = fromMem
+  except:
+    result = 0
+    print "Error in reading memory (attitude.ReadPitch)"
 
   return result
