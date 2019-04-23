b47cddb55e6a6ac8c1ef4cec4e85c3b00b5ae709
adibyhy <adibyhy@Desktop-Mint>
2019-04-23 19:53:14 -0400

    Read PWM status and display on GUI

--- firstflight-drone/gui/qt/gui.py
+++ firstflight-drone/gui/qt/gui.py
@@ -4,6 +4,7 @@ from PyQt4.QtGui import *
 from mainWindow import Ui_MainWindow
 from diagnostics import Ui_widget_diagnostics
 from adc import ReadADC
+from pwm import ReadPWM
 
 class MainWindow(QMainWindow):
   def __init__(self):
@@ -36,6 +37,26 @@ class MainWindow(QMainWindow):
   def tick(self):
     busVoltage = ReadADC()
     self.diagnostics.label_busVoltVal.setText(busVoltage)
+    self.updatePWM()
+
+  def updatePWM(self):
+    motor1_duty = ReadPWM(1)
+    self.diagnostics.label_motor1Val.setText(str(motor1_duty))
+
+    motor2_duty = ReadPWM(2)
+    self.diagnostics.label_motor2Val.setText(str(motor2_duty))
+
+    motor3_duty = ReadPWM(3)
+    self.diagnostics.label_motor3Val.setText(str(motor3_duty))
+
+    motor4_duty = ReadPWM(4)
+    self.diagnostics.label_motor4Val.setText(str(motor4_duty))
+
+    motor_armed = ReadPWM(5)
+    if (motor_armed == 0):
+      self.diagnostics.label_arm.setText("Disarmed")
+    else:
+      self.diagnostics.label_arm.setText("Armed")
 
 def main():
   app = QApplication(sys.argv)
---
+++ firstflight-drone/gui/qt/pwm.py
@@ -0,0 +1,48 @@
+import sys
+import argparse
+import time
+import mmap
+import struct
+
+PWM_ADDRESS = 0x43c10000
+
+def ReadPWM(whichPort):
+  try:
+    f = open("/dev/mem", "r+b")
+    mem = mmap.mmap(f.fileno(), 32, offset=PWM_ADDRESS)
+
+    if (whichPort == 1):
+      # Motor 1
+      reg = 4
+    elif (whichPort == 2):
+      # Motor 2
+      reg = 8
+    elif (whichPort == 3):
+      # Motor 3
+      reg = 12
+    elif (whichPort == 4):
+      # Motor 4
+      reg = 16
+    else:
+      # Read enable
+      reg = 0
+
+    mem.seek(reg)
+    fromMem = struct.unpack('l', mem.read(4))[0]
+
+    # Convert to percentage is reading motor
+    if (whichPort == 1 or whichPort == 2 or whichPort == 3 or whichPort == 4):
+      fromMem = int((float(fromMem)/float(102040))*100)
+
+    # print str(reg) + " = " + str(fromMem)
+
+    # Close mem after finishing
+    mem.close()
+    f.close()
+
+    result = fromMem
+  except:
+    result = 0
+    print "Error in reading memory (pwm.ReadPWM)"
+
+  return result
